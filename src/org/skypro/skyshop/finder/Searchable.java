bab9420da3a06ce5b6a8c780948225d331b4a32b
bannikov_ilya <101068317+b4ntik@users.noreply.github.com>
2025-04-16 22:09:17 +0500

    homeWork_3.8

--- src/org/skypro/skyshop/finder/Searchable.java
+++ src/org/skypro/skyshop/finder/Searchable.java
@@ -3,19 +3,13 @@ package org.skypro.skyshop.finder;
 public interface Searchable {
     default String searchTerm() {
         return "";
-    }
-
-    ;
+    };
 
     default String getProductType() {
         return "";
-    }
-
-    ;
+    };
 
     default String getStringRepresentation() {
         return "";
-    }
-
-    ;
+    };
 }
