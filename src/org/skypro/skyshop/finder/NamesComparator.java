05b906a1147b8c855a835d03258d65b9949888fb
bannikov_ilya <101068317+b4ntik@users.noreply.github.com>
2025-06-15 00:03:59 +0500

    homeWork_7.2_refactorForSet

--- src/org/skypro/skyshop/finder/NamesComparator.java
+++ src/org/skypro/skyshop/finder/NamesComparator.java
@@ -2,16 +2,16 @@ package org.skypro.skyshop.finder;
 import java.util.Comparator;
 
 public class NamesComparator implements Comparator<Integer>{
-    @Override
+
     public int compare(Searchable o1, Searchable o2){
 
         return 0;
     }
 
-    /*@Override
+    @Override
     public int compare(Integer o1, Integer o2) {
         return 0;
-    }*/
+    }
 
     @Override
     public Comparator<Integer> reversed() {
